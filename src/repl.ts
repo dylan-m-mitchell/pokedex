3829e71fac61c49a141e49062fb38c154d8bdd97
Dylan Mitchell <dlnmtchll@gmail.com>
2025-08-27 23:53:30 -0500

    feat: enhance command handling with exit confirmation and error message for unknown commands

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,5 @@
 import { createInterface } from "node:readline";
-import { CLICommand, getCommands } from "./commands";
-import { commandExit } from "./command_exit";
+import { getCommands } from "./commands";
 
 export function cleanInput(input: string): string[] {
   // split strings into array
@@ -20,17 +19,22 @@ export function startREPL() {
   rl.on("line", (raw: string) => {
     const word: string = handleLine(raw);
     const commands = getCommands();
+    // console.log(commands["exit"]);
 
     if (+word === 0) {
       rl.prompt();
       return;
 
     }
-    else if (word in commands) {
-      const command: CLICommand = commands[word];
-      command.callback();
+    else if (commands[word]) {
+      // const command: CLICommand = commands[word];
+      commands[word].callback(commands);
       rl.prompt();
       return;
+    } else {
+        console.log("Unknown command");
+        rl.prompt();
+        return;
     }
   });
 }
